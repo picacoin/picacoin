79e4e915e0de5f054636d37c13de16667c8c1269
picacoin <84209422+picacoin@users.noreply.github.com>
2021-06-11 15:54:01 -0700

    Update warnings.cpp

--- src/warnings.cpp
+++ src/warnings.cpp
@@ -37,8 +37,8 @@ bilingual_str GetWarnings(bool verbose)
 
     // Pre-release build warning
     if (!CLIENT_VERSION_IS_RELEASE) {
-        warnings_concise = _("This is a pre-release test build - use at your own risk - do not use for mining or merchant applications");
-        warnings_verbose.emplace_back(warnings_concise);
+        //warnings_concise = _("This is a pre-release test build - use at your own risk - do not use for mining or merchant applications");
+        //warnings_verbose.emplace_back(warnings_concise);
     }
 
     // Misc warnings like out of disk space and clock is wrong
